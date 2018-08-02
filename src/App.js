201caf32ae99a5027b55add27f6d632f8c175c64
Makoto <inouemak@googlemail.com>
2018-08-02 15:33:25 +0100

    Display dns lookup result

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
+import DNSRegistrarContract from '@ensdomains/dnsregistrar/build/contracts/DNSRegistrar.json'
+import namehash from 'eth-ens-namehash';
+const DNSRegistrarJS = require('@ensdomains/dnsregistrar');
 import getWeb3 from './utils/getWeb3'
 
 import './css/oswald.css'
@@ -12,9 +14,12 @@ class App extends Component {
     super(props)
 
     this.state = {
-      storageValue: 0,
+      domain: 'matoken.xyz',
+      owner:null,
       web3: null
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillMount() {
@@ -35,6 +40,15 @@ class App extends Component {
     })
   }
 
+  handleChange(event) {
+    this.setState({domain: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.instantiateContract();
+    event.preventDefault();
+  }
+
   instantiateContract() {
     /*
      * SMART CONTRACT EXAMPLE
@@ -44,26 +58,24 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const DNSRegistrar = contract(DNSRegistrarContract);
+    DNSRegistrar.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var registrarjs
 
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+      DNSRegistrar.deployed().then((registrar)=>{
+        registrarjs = new DNSRegistrarJS(this.state.web3.currentProvider, registrar.address);
+      }).then(()=>{
+        return registrarjs.claim(this.state.domain);
+      }).then((claim)=>{
+        let text ='Not found';
+        if(claim.found){
+          text = claim.getOwner();
+        } 
+        return this.setState({ owner: text })
+      })  
     })
   }
 
@@ -78,11 +90,13 @@ class App extends Component {
           <div className="pure-g">
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
-              <p>The stored value is: {this.state.storageValue}</p>
+              <p>Your ENS is installed and ready.</p>
+              <h2>DNS Integration Example</h2>
+              <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.domain} onChange={this.handleChange} />
+              <input type="submit" value="Submit" />
+              <p>The {this.state.domain} is owned by : {this.state.owner}</p>
+              </form>
             </div>
           </div>
         </main>
